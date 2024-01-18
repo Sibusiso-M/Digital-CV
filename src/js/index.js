1188d8d1c813182f08cf7ae974c6b14de5ffbf9b
Sibu <sibu.mdlovu@gmail.com>
2024-01-18 19:44:56 +0200

    hide and show menu function

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,27 @@
 // Get all the elements for the hamburger
-// let navLinks = document.getElementById('.navLinks');
 const hamburgerBar = document.getElementById("hamburger-bar");
 const hamburgerBarCross = document.getElementById("hamburger-bar-cross");
-// let hamburgerBarCross = document.querySelector('#hamburger-bar');
+const navMenu = document.getElementById("nav-menu");
+
 // On click toggle the class on the hamburger
 
 hamburgerBar.classList.add("open");
 hamburgerBarCross.classList.add("close");
+navMenu.classList.add("close");
 
 hamburgerBar.onclick = () => {
   console.log("click hamburgerBar");
-  // navLinks.classList.toggle('open');
   hamburgerBar.classList.remove("open");
   hamburgerBar.classList.add("close");
   hamburgerBarCross.classList.add("open");
+  navMenu.classList.add("open");
 };
 
 hamburgerBarCross.onclick = () => {
   console.log("click hamburgerBarCross");
-  // navLinks.classList.toggle('open');
   hamburgerBarCross.classList.add("close");
   hamburgerBarCross.classList.remove("open");
   hamburgerBar.classList.remove("close");
   hamburgerBar.classList.add("open");
+  navMenu.classList.remove("open");
 };
