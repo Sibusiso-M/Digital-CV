7334d57f2aa7a08167b5bc1c7759eaf503582da6
Sibu <sibu.mdlovu@gmail.com>
2024-01-25 15:17:35 +0200

    update queries

--- src/helper-objects/dbQueries.js
+++ src/helper-objects/dbQueries.js
@@ -1,9 +1,14 @@
 const databaseQuery = {
   createDatabase: "",
-  createTable:
-    "CREATE TABLE IF NOT EXISTS visitors (visitorId SERIAL, firstName varchar(256), lastName varchar(256), date_of_visit DATE, time_of_visit TIME, message varchar(256), PRIMARY KEY(visitorId)",
+  createAdminTable:
+    "CREATE TABLE IF NOT EXISTS administrator (adminId SERIAL, password varchar(256)",
 };
 
-const contactQueries = {};
+const contactQueries = {
+  createVisitorsTable:
+    "CREATE TABLE IF NOT EXISTS visitors (visitorId SERIAL, firstName varchar(256), lastName varchar(256), dateOfVisit DATE, timeOfVisit TIME, message varchar(256), PRIMARY KEY(visitorId)",
+  addAVisitor:
+    "INSERT INTO visitors(firstName, lastName, dateOfVisit, timeOfVisit, email, message) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
+};
 
 const adminQueries = {};
