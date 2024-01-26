7462201a7a74338d0cb8ca24d7badb6dcb4e7cc8
Sibu <sibu.mdlovu@gmail.com>
2024-01-26 18:57:42 +0200

    add basic new_visitor endpont

--- src/routes/digital-cv.js
+++ src/routes/digital-cv.js
@@ -11,5 +11,9 @@ const sendFileHandler = (fileName) => (request, response) => {
 };
 
 router.get("/", sendFileHandler("index.html"));
+router.post("/new_visitor_post", (request, response) => {
+  console.log("new visitor post attempted");
+  response.send("hello");
+});
 
 module.exports = router;
