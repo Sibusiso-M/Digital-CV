d200fd6ee8240b3f2cf74734607c35267bb2f09c
Sibu <sibu.mdlovu@gmail.com>
2024-01-26 19:23:47 +0200

    export create table function

--- src/js/new-visitor.js
+++ src/js/new-visitor.js
@@ -1,4 +1,20 @@
-const { contactQueries } = require("../helper-objects/databaseQueries");
+const { visitorQueries } = require("../helper-objects/databaseQueries");
 const { pool } = require("../configurations");
 const { errorMessage } = require("../helper-objects/error-messages");
 
+const queryFunctions = async (query, values = []) => {
+  let visitors;
+  if (query !== undefined) {
+    visitors = await pool.query(query, values);
+    return visitors.rows;
+  } else {
+    throw new Error(errorMessage.emptyQuery);
+  }
+};
+
+const createTable = async () => {
+  console.log("create table attempted");
+  return await queryFunctions(visitorQueries.createVisitorsTable);
+};
+
+module.exports = { createTable };
