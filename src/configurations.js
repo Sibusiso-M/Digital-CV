fbb06ac4fbe0fa93ce72bb5b76dcd06cd06c9683
Sibu <sibu.mdlovu@gmail.com>
2024-01-28 12:06:39 +0200

    add defaults for testing

--- src/configurations.js
+++ src/configurations.js
@@ -4,9 +4,9 @@ dotenv.config();
 const { Pool } = require("pg");
 const pool = new Pool({
   host: "localhost",
-  database: process.env.DATABASE,
-  port: process.env.PORT,
-  user: process.env.USER,
+  database: process.env.DATABASE || "db",
+  port: process.env.PORT || 5432,
+  user: process.env.USER  || "user",
   PASSWORD: process.env.PASSWORD,
 });
 
