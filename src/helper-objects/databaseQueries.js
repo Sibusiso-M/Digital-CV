b6190f388e5bacc6a142ee83c3ba09a2063f83ef
Sibu <sibu.mdlovu@gmail.com>
2024-01-28 22:58:31 +0200

    fix primary key

--- src/helper-objects/databaseQueries.js
+++ src/helper-objects/databaseQueries.js
@@ -1,12 +1,12 @@
 const databaseQuery = {
   createDatabase: "CREATE DATABASE 'DigitalCVAppDatabase'",
   createAdminTable:
-    "CREATE TABLE IF NOT EXISTS administrator (adminId SERIAL, password varchar(256)",
+    "CREATE TABLE IF NOT EXISTS administrator (adminId SERIAL PRIMARY KEY, password varchar(256)",
 };
 
 const visitorQueries = {
   createVisitorsTable:
-    "CREATE TABLE IF NOT EXISTS visitors (visitorId SERIAL, firstName varchar(256), lastName varchar(256), dateOfVisit DATE, timeOfVisit TIME, message varchar(256), PRIMARY KEY(visitorId));",
+    "CREATE TABLE IF NOT EXISTS visitors (visitorId SERIAL PRIMARY KEY, firstName varchar(256), lastName varchar(256), dateOfVisit DATE, timeOfVisit TIME, message varchar(256));",
   addAVisitor:
     "INSERT INTO visitors(firstName, lastName, dateOfVisit, timeOfVisit, email, message) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
 };
