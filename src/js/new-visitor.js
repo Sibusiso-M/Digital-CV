ba446105efc0da6b498f90694fa7157681dd0faf
Sibu <sibu.mdlovu@gmail.com>
2024-01-28 16:30:38 +0200

    try old function

--- src/js/new-visitor.js
+++ src/js/new-visitor.js
@@ -2,14 +2,28 @@ const { visitorQueries } = require("../helper-objects/databaseQueries");
 const { pool } = require("../configurations");
 const { errorMessage } = require("../helper-objects/error-messages");
 
-const queryFunctions = async (query, values = []) => {
-  try {
-    if (query === undefined) throw new Error(errorMessage.emptyQuery);
-    const { rows } = await pool.query(query, values);
-    return rows;
-  } catch (error) {
-    console.log("Error executing query:", error.message);
-    throw error;
+// const queryFunctions = async (query, values = []) => {
+//   try {
+//     if (query === undefined) throw new Error(errorMessage.emptyQuery);
+//     const { rows } = await pool.query(query, values);
+//     return rows;
+//   } catch (error) {
+//     console.log("Error executing query:", error.message);
+//     throw error;
+//   }
+// };
+
+const queryFunctions = async (query, values) => {
+  let visitors;
+  if (query !== undefined) {
+    if (values === undefined) {
+      visitors = await pool.query(query);
+    } else {
+      visitors = await pool.query(query, values);
+    }
+    return visitors.rows;
+  } else {
+    throw new Error(errorMessage.emptyQuery);
   }
 };
 
