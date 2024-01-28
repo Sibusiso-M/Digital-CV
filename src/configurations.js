596ec48b2c139fb98736fddc8896e617c334a867
Sibu <sibu.mdlovu@gmail.com>
2024-01-28 13:51:51 +0200

    add defult pass

--- src/configurations.js
+++ src/configurations.js
@@ -6,8 +6,8 @@ const pool = new Pool({
   host: "localhost",
   database: process.env.DATABASE || "db",
   port: process.env.PORT || 5432,
-  user: process.env.USER  || "user",
-  PASSWORD: process.env.PASSWORD,
+  user: process.env.USER || "user",
+  PASSWORD: process.env.PASSWORD || "pass",
 });
 
 module.exports = { pool };
