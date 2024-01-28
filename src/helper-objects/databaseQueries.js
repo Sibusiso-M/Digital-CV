c1157f92ef1d104968b5b56af82ebf045d086cff
Sibu <sibu.mdlovu@gmail.com>
2024-01-28 16:29:16 +0200

    update query

--- src/helper-objects/databaseQueries.js
+++ src/helper-objects/databaseQueries.js
@@ -6,7 +6,7 @@ const databaseQuery = {
 
 const visitorQueries = {
   createVisitorsTable:
-    "CREATE TABLE IF NOT EXISTS visitors (visitorId SERIAL, firstName varchar(256), lastName varchar(256), dateOfVisit DATE, timeOfVisit TIME, message varchar(256), PRIMARY KEY(visitorId)",
+    "CREATE TABLE IF NOT EXISTS visitors (visitorId SERIAL, firstName varchar(256), lastName varchar(256), dateOfVisit DATE, timeOfVisit TIME, message varchar(256), PRIMARY KEY(visitorId));",
   addAVisitor:
     "INSERT INTO visitors(firstName, lastName, dateOfVisit, timeOfVisit, email, message) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
 };
