eb1ff83c2044fb90951064bdae165550b20b2c93
Sibu <sibu.mdlovu@gmail.com>
2024-01-28 12:05:44 +0200

    spacing

--- express.js
+++ express.js
@@ -11,7 +11,6 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(express.static(path.join(__dirname)));
 app.use("", express.static(path.join(__dirname, "public")));
 app.use("/static", express.static(path.join(__dirname, "src")));
