5749e31079f21e0a14b1eb3a226596fd6b651775
Sibu <sibu.mdlovu@gmail.com>
2024-02-01 00:53:36 +0200

    clean up file fix form logic;

---
+++ index.js
@@ -0,0 +1,134 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("contact-me-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      resetFormFields();
+      submitForm();
+    });
+
+  document.getElementById("successMessage").style.display = "none";
+  const hamburgerBar = document.getElementById("hamburger-bar");
+  const hamburgerBarCross = document.getElementById("hamburger-bar-cross");
+  const navMenu = document.getElementById("nav-menu");
+
+  hamburgerBar.classList.add("open");
+  hamburgerBarCross.classList.add("close");
+  navMenu.classList.add("close");
+
+  hamburgerBar.onclick = () => {
+    hamburgerBar.classList.remove("open");
+    hamburgerBar.classList.add("close");
+    hamburgerBarCross.classList.add("open");
+    navMenu.classList.add("open");
+  };
+
+  const navMenuHide = () => {
+    hamburgerBarCross.classList.add("close");
+    hamburgerBarCross.classList.remove("open");
+    hamburgerBar.classList.remove("close");
+    hamburgerBar.classList.add("open");
+    navMenu.classList.remove("open");
+  };
+
+  hamburgerBarCross.onclick = () => {
+    navMenuHide();
+  };
+
+  let navLinks = navMenu.querySelectorAll("a");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navMenuHide();
+    });
+  });
+
+  function submitForm() {
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    emailAddress = document.getElementById("emailAddress").value;
+    message = document.getElementById("message").value;
+
+    const currentDateAndTime = new Date();
+
+    const year = currentDateAndTime.getFullYear();
+    const month = currentDateAndTime.getMonth() + 1;
+    const day = currentDateAndTime.getDate();
+
+    const hours = currentDateAndTime.getHours();
+    const minutes = currentDateAndTime.getMinutes();
+    const seconds = currentDateAndTime.getSeconds();
+    const dateOfVisit = `${year}-${month}-${day}`;
+    const timeOfVisit = `${hours}:${minutes}:${seconds}`;
+
+    const formData = {
+      firstName,
+      lastName,
+      emailAddress,
+      message,
+      dateOfVisit,
+      timeOfVisit,
+    };
+
+    fetch("/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const successMessage = (document.getElementById(
+          "successMessage"
+        ).style.display = "flex");
+
+        if (data.success) {
+          successMessage.style.display = "flex";
+        } else {
+          successMessage.innerHTML =
+            "❌ Form submission failed, please try again.";
+          console.error(`Form submission failed: ${data.message}`);
+        }
+      })
+      .catch((error) => {
+        console.error(`Contact Form submission failed: ${error}`);
+      });
+  }
+
+  const digitalCVMenu = document.getElementById("digital-cv-nav");
+
+  let digitalCVMenuHeight = digitalCVMenu.offsetHeight;
+
+  let lastScrollY = window.scrollY;
+
+  // window.onscroll = function () {
+  //   const currentScrollY = window.scrollY;
+
+  //   if (lastScrollY < currentScrollY && currentScrollY > digitalCVMenuHeight) {
+  //     digitalCVMenu.classList.add("hide");
+  //     // digitalCVBody.style.top = -digitalCVMenuHeight;
+  //   } else {
+  //     digitalCVMenu.classList.remove("hide");
+  //   }
+
+  //   lastScrollY = currentScrollY;
+  // };
+
+  const digitalCVBody = document.getElementById("digital-cv-body");
+  digitalCVBody.style.position = "relative";
+
+  digitalCVBody.style.top = digitalCVMenuHeight + "px";
+
+  const visitorClearFieldsButton = document.getElementById(
+    "visitorClearFieldsButton"
+  );
+
+  visitorClearFieldsButton.onclick = () => {
+    resetFormFields();
+  };
+
+  function resetFormFields() {
+    document.getElementById("contact-me-form").reset();
+  }
+});
