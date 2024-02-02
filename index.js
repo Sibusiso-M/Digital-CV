1579484b50275b7b712c6c43d87d79ab0371b582
Sibu Mdlovu <Sibusiso.Mdlovu@bbd.co.za>
2024-02-02 11:09:50 +0200

    allow explicit server side scripting

--- index.js
+++ index.js
@@ -1,117 +1,118 @@
 document.addEventListener("DOMContentLoaded", function () {
-document
-  .getElementById("contact-me-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    submitForm();
-    resetFormFields();
-  });
+  document
+    .getElementById("contact-me-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      submitForm();
+      resetFormFields();
+    });
 
-document.getElementById("successMessage").style.display = "none";
-const hamburgerBar = document.getElementById("hamburger-bar");
-const hamburgerBarCross = document.getElementById("hamburger-bar-cross");
-const navMenu = document.getElementById("nav-menu");
+  document.getElementById("successMessage").style.display = "none";
+  const hamburgerBar = document.getElementById("hamburger-bar");
+  const hamburgerBarCross = document.getElementById("hamburger-bar-cross");
+  const navMenu = document.getElementById("nav-menu");
 
-hamburgerBar.classList.add("open");
-hamburgerBarCross.classList.add("close");
-navMenu.classList.add("close");
-
-hamburgerBar.onclick = () => {
-  hamburgerBar.classList.remove("open");
-  hamburgerBar.classList.add("close");
-  hamburgerBarCross.classList.add("open");
-  navMenu.classList.add("open");
-};
-
-const navMenuHide = () => {
-  hamburgerBarCross.classList.add("close");
-  hamburgerBarCross.classList.remove("open");
-  hamburgerBar.classList.remove("close");
   hamburgerBar.classList.add("open");
-  navMenu.classList.remove("open");
-};
+  hamburgerBarCross.classList.add("close");
+  navMenu.classList.add("close");
 
-hamburgerBarCross.onclick = () => {
-  navMenuHide();
-};
-
-let navLinks = navMenu.querySelectorAll("a");
-
-navLinks.forEach(function (link) {
-  link.addEventListener("click", function () {
-    navMenuHide();
-  });
-});
-
-async function submitForm() {
-  const currentDateAndTime = new Date();
-
-  const year = currentDateAndTime.getFullYear();
-  const month = currentDateAndTime.getMonth() + 1;
-  const day = currentDateAndTime.getDate();
-
-  const hours = currentDateAndTime.getHours();
-  const minutes = currentDateAndTime.getMinutes();
-  const seconds = currentDateAndTime.getSeconds();
-  const dateOfVisit = `${year}-${month}-${day}`;
-  const timeOfVisit = `${hours}:${minutes}:${seconds}`;
-  
-  const formData = {
-    firstName: document.getElementById("firstName").value,
-    lastName: document.getElementById("lastName").value,
-    emailAddress: document.getElementById("emailAddress").value,
-    dateOfVisit: dateOfVisit,
-    timeOfVisit: timeOfVisit,
+  hamburgerBar.onclick = () => {
+    hamburgerBar.classList.remove("open");
+    hamburgerBar.classList.add("close");
+    hamburgerBarCross.classList.add("open");
+    navMenu.classList.add("open");
   };
 
-  fetch("https://sibusiso-mdlovu-digital-cv.netlify.app/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const successMessage = (document.getElementById(
-        "successMessage"
-      ).style.display = "flex");
-      if (data.success) {
-        successMessage.innerHTML =
-          "Form submitted successfully!⚡ We will be in touch. 🤝🏼";
-      } else {
-        successMessage.innerHTML =
-          "❌ Form submission failed, please try again.";
-        console.error(`Form submission failed: ${data}`);
-      }
+  const navMenuHide = () => {
+    hamburgerBarCross.classList.add("close");
+    hamburgerBarCross.classList.remove("open");
+    hamburgerBar.classList.remove("close");
+    hamburgerBar.classList.add("open");
+    navMenu.classList.remove("open");
+  };
 
-      setTimeout(() => {
-        successMessage.innerHTML = null;
-      }, 4000);
-    })
-    .catch((error) => {
-      console.error(`Contact Form submission failed: ${error}`);
+  hamburgerBarCross.onclick = () => {
+    navMenuHide();
+  };
+
+  let navLinks = navMenu.querySelectorAll("a");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navMenuHide();
     });
-}
+  });
 
-const digitalCVMenu = document.getElementById("digital-cv-nav");
+  async function submitForm() {
+    const currentDateAndTime = new Date();
 
-let digitalCVMenuHeight = digitalCVMenu.offsetHeight;
+    const year = currentDateAndTime.getFullYear();
+    const month = currentDateAndTime.getMonth() + 1;
+    const day = currentDateAndTime.getDate();
 
-const digitalCVBody = document.getElementById("digital-cv-body");
-digitalCVBody.style.position = "relative";
+    const hours = currentDateAndTime.getHours();
+    const minutes = currentDateAndTime.getMinutes();
+    const seconds = currentDateAndTime.getSeconds();
+    const dateOfVisit = `${year}-${month}-${day}`;
+    const timeOfVisit = `${hours}:${minutes}:${seconds}`;
 
-digitalCVBody.style.top = digitalCVMenuHeight + "px";
+    const formData = {
+      firstName: document.getElementById("firstName").value,
+      lastName: document.getElementById("lastName").value,
+      emailAddress: document.getElementById("emailAddress").value,
+      dateOfVisit: dateOfVisit,
+      timeOfVisit: timeOfVisit,
+    };
 
-const visitorClearFieldsButton = document.getElementById(
-  "visitorClearFieldsButton"
-);
+    fetch("https://sibusiso-mdlovu-digital-cv.netlify.app/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const successMessage = (document.getElementById(
+          "successMessage"
+        ).style.display = "flex");
+        if (data.success) {
+          successMessage.innerHTML =
+            "Form submitted successfully!⚡ We will be in touch. 🤝🏼";
+        } else {
+          successMessage.innerHTML =
+            "❌ Form submission failed, please try again.";
+          console.error(`Form submission failed: ${data}`);
+        }
 
-visitorClearFieldsButton.onclick = () => {
-  resetFormFields();
-};
+        setTimeout(() => {
+          successMessage.innerHTML = null;
+        }, 4000);
+      })
+      .catch((error) => {
+        console.error(`Contact Form submission failed: ${error}`);
+      });
+  }
 
-function resetFormFields() {
-  document.getElementById("contact-me-form").reset();
-}
+  const digitalCVMenu = document.getElementById("digital-cv-nav");
+
+  let digitalCVMenuHeight = digitalCVMenu.offsetHeight;
+
+  const digitalCVBody = document.getElementById("digital-cv-body");
+  digitalCVBody.style.position = "relative";
+
+  digitalCVBody.style.top = digitalCVMenuHeight + "px";
+
+  const visitorClearFieldsButton = document.getElementById(
+    "visitorClearFieldsButton"
+  );
+
+  visitorClearFieldsButton.onclick = () => {
+    resetFormFields();
+  };
+
+  function resetFormFields() {
+    document.getElementById("contact-me-form").reset();
+  }
 });
